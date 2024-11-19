b1bcb0349a6a222ba6ad6717fd95822d605ae18a
JulianGan <88633987+JulianGan@users.noreply.github.com>
2024-11-19 18:57:20 -0500

    Update insertionQ.cu

--- insertionQ.cu
+++ insertionQ.cu
@@ -6,97 +6,210 @@
 
 #define RANGE 99.0
 
-void push(float * queue, float item, int max_size);
-__global__ void d_push(float * queue, float item, int max_size);
-float pop(float * queue, int max_size);
-__global__ void d_pop(float * queue, int max_size, int ofs);
+// --- GLOBAL VARIABLE ---
+int size = 0, max_size = 0;
+float * d_queue, * buffer;
 
-__device__ float min(float * old, float val){
-    if (val < *old){
-        float temp = *old;
-        *old = val;
-        return temp;
-    } else {
-        return *old;
-    }
+// --- FUNCTION DECLARATION ---
+void init_queue(int n);
+void free_queue();
+void push(float item);
+__global__ void find_index(float * d_queue, float item, int * d_ret, int max_size, int ofs);
+__global__ void d_push(float * d_queue, float * buffer, float item, int index, int max_size, int ofs);
+float pop();
+__global__ void d_pop(float * d_queue, float * buffer, float * d_ret, int max_size, int size, int ofs);
+
+// --- UTILITY FUNCTION ---
+void swap(float * &a, float * &b){
+    float * temp = a;
+    a = b;
+    b = temp;
 }
 
-void print_queue(float * queue, int n){
-    for (int i = 0; i < n; i++){
-        printf("%f ", queue[i]);
-        if ((i != 0) && (i % 10 == 0)) printf("\n");
-    }
-    if (n % 10 != 0) printf("\n");
+float random_float(){
+    return ((float)rand()/(float)(RAND_MAX)) * RANGE;
 }
 
-void run_tests(float * queue, int max_size){
-    float item;
+void print_queue(){
+    float * temp;
+    temp = (float *) malloc(max_size * sizeof(float));
+    cudaMemcpy(temp, d_queue, max_size * sizeof(float), cudaMemcpyDeviceToHost);
     for (int i = 0; i < max_size; i++){
-        item = ((float)rand()/(float)(RAND_MAX)) * RANGE;
-        push(queue, item, max_size);
+        printf("%f ", temp[i]);
     }
-    print_queue(queue, max_size);
+    printf("\n");
+    free(temp);
+}
+
+void run_tests(int ops, int ratio){
+
+    int i; // loop index
+
+    for (i = 0; i < int(ops * (ratio / 100.0)); i++){
+        push( random_float() );
+    }
+
+    for (i = 0; i < int(ops * (1 - ratio / 100.0)); i++){
+        pop();
+    }
+    
+    #ifdef DEBUG
+    print_queue();
+    #endif
+
 }
 
 int main(int argc, char *argv[]){
 
     cudaError_t err;
-    int n = 0;
+    int ops = 0, ratio = 0;
 
-    if(argc != 2){
-		printf("usage:  ./insertionQ n\n");
-		printf("n = number of elements in array\n");
+    if(argc != 4){
+		printf("usage: ./insertionQ <total_operations> <ratio_of_push> <device_number>\n");
+		printf("total_operations = number of push/pop operations to simulate\n");
+        printf("ratio_of_push = percent of operations that are the push operation * 100\n");
+        printf("device_number = index of device (0 or 1 on cims machine)\n");
 		exit(1);
 	}
 
-    n = atoi(argv[1]);
-
-    float * queue;
-    err = cudaMallocManaged((void **) &queue, n * sizeof(float));
+    err = cudaSetDevice(atoi(argv[3]));
     if (err != cudaSuccess){
-        printf("queue could not be allocated, exiting...");
+        int deviceCount;
+        cudaGetDeviceCount(&deviceCount);
+        printf("available devices: %d\n", deviceCount);
+        printf("error: could not set device %d\n", atoi(argv[2]));
         exit(1);
     }
 
-    for (int i = 0; i < n; i++){
-        queue[i] = INFINITY;
-    }
+    ops = atoi(argv[1]);
+    ratio = atoi(argv[2]);
 
-    run_tests(queue, n);
+    init_queue(ops);
 
-    cudaFree(queue);
+    run_tests(ops, ratio);
+
+    free_queue();
 
     return 0;
 }
 
-void push(float * queue, float item, int max_size){
-    d_push<<<1, 1>>>(queue, item, max_size);
-}
+// --- FUNCTION DEFINITION ---
+void init_queue(int n){
 
-__global__ void d_push(float * queue, float item, int max_size){
-    int i;
-    float old;
-    for (i = 0; i < max_size; i++){
-        old = min(&queue[i], item);
-        if (isinf(old)) break;
-        if (old > item) item = old;
+    cudaError_t err;
+
+    max_size = n;
+
+    err = cudaMalloc((void **) &d_queue, n * sizeof(float));
+    if (err != cudaSuccess){
+        printf("error: queue could not be allocated, exiting...\n");
+        exit(1);
     }
+    err = cudaMalloc((void **) &buffer, n * sizeof(float));
+    if (err != cudaSuccess){
+        printf("error: queue could not be allocated, exiting...\n");
+        exit(1);
+    }
+
+    do {
+        float * temp;
+        temp = (float *) malloc(n * sizeof(float));
+        for (int i = 0; i < n; i++){
+            temp[i] = INFINITY;
+        }
+        err = cudaMemcpy(d_queue, temp, n * sizeof(float), cudaMemcpyHostToDevice);
+        free(temp);
+    } while (0);
 }
 
-float pop(float * queue, int max_size){
-    cudaDeviceSynchronize();
-    float val = queue[0];
-    dim3 gridDim(128);
+void free_queue(){
+    cudaFree(d_queue); cudaFree(buffer);
+}
+
+void push(float item){
+
+    dim3 gridDim(32);
     dim3 blockDim(250);
-    for (int i = 0; i < ceil(max_size / (gridDim.x * blockDim.x)); i++){
-        d_pop<<<gridDim, blockDim>>>(queue, max_size, i * gridDim.x * blockDim.x);
+
+    int * d_ret, index, ofs;
+    cudaMalloc((void **) &d_ret, sizeof(int));
+    for (ofs = 0; ofs < max_size; ofs += gridDim.x * blockDim.x){
+        find_index<<<gridDim, blockDim>>>(d_queue, item, d_ret, max_size, ofs);
+    }
+    cudaMemcpy(&index, d_ret, sizeof(int), cudaMemcpyDeviceToHost);
+
+    for (ofs = 0; ofs < max_size; ofs += gridDim.x * blockDim.x){
+        d_push<<<gridDim, blockDim>>>(d_queue, buffer, item, index, max_size, ofs);
+    }
+    ++size;
+    cudaFree(d_ret);
+    swap(d_queue, buffer);
+}
+
+__global__ void find_index(float * d_queue, float item, int * d_ret, int max_size, int ofs){
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    if (tid + ofs >= max_size) return;
+
+    if (tid + ofs == 0){
+        if (item < d_queue[0]){
+            *d_ret = 0;
+        }
+        return;
+    }
+    if (d_queue[tid + ofs - 1] <= item && item < d_queue[tid + ofs]){
+        *d_ret = tid + ofs;
     }
 }
 
-__global__ void d_pop(float * queue, int max_size, int ofs){
+__global__ void d_push(float * d_queue, float * buffer, float item, int index, int max_size, int ofs){
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    float temp = queue[tid + ofs];
-    cudaThreadSynchronize();
-    if (tid + ofs == 0) return;
-    queue[tid + ofs - 1] = temp;
+    if (tid + ofs >= max_size) return;
+
+    if (tid + ofs > index){
+        buffer[tid + ofs] = d_queue[tid + ofs - 1]; return;
+    }
+    if (tid + ofs < index){
+        buffer[tid + ofs] = d_queue[tid + ofs]; return;
+    }
+    if (tid + ofs == index){
+        buffer[tid + ofs] = item;
+    }
+
+}
+
+float pop(){
+
+    dim3 gridDim(32);
+    dim3 blockDim(250);
+
+    int ofs;
+    float * d_ret;
+    cudaMalloc((void **) &d_ret, sizeof(float));
+    for (ofs = 0; ofs < max_size; ofs += gridDim.x * blockDim.x){
+        d_pop<<<gridDim, blockDim>>>(d_queue, buffer, d_ret, max_size, size, ofs);
+    }
+    --size;
+
+    float temp;
+    cudaMemcpy(&temp, d_ret, sizeof(float), cudaMemcpyDeviceToHost);
+    cudaFree(d_ret);
+    swap(d_queue, buffer);
+    return temp;
+}
+
+__global__ void d_pop(float * d_queue, float * buffer, float * d_ret, int max_size, int size, int ofs){
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    if (tid + ofs >= max_size) return;
+
+    if (tid + ofs == 0){
+        *d_ret = d_queue[0];
+        return;
+    }
+
+    buffer[tid + ofs - 1] = d_queue[tid + ofs];
+
+    if (tid + ofs == size - 1){
+        buffer[tid + ofs] = INFINITY;
+        d_queue[tid + ofs] = INFINITY;
+    }
 }
